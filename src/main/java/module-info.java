9b0bbaa415c4c2d96e422b4cf466b206dd79c18d
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-02-25 14:50:30 +0100

    Delete

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.polimi.progetto_ingsoft {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens it.polimi.progetto_ingsoft to javafx.fxml;
-    exports it.polimi.progetto_ingsoft;
-}
