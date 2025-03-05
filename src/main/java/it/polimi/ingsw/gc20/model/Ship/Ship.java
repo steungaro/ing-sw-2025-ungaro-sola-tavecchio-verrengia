87a9deaef1322b0acaf81ec326f24c738968a5a7
AlessandroSola <alessandrosola03@gmail.com>
2025-03-05 19:10:55 +0100

    Fatto tile manca costruttore default Qualcosa fatto su ship

--- src/main/java/it/polimi/ingsw/gc20/model/Ship/Ship.java
+++ src/main/java/it/polimi/ingsw/gc20/model/Ship/Ship.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.gc20.model.Ship;
 
 import Components.Component;
+import it.polimi.ingsw.gc20.model.Components.Component;
 
 import java.io.*;
 import java.util.*;
@@ -21,6 +22,7 @@ public abstract class Ship {
      */
     private Set<Component> waste;
 
+    private Integer singleEngines;
     /**
      * 
      */
@@ -42,26 +44,32 @@ public abstract class Ship {
     private Float singleCannonsPower;
 
     /**
+     * Function that determines the total fire power of the ship
+     * it is the sum of single cannons power and double cannons power based on their orientation (cannons facing north have full power, others have half power)
+     * the user will select one by one the cannons he wants to use (single cannons automatically selected) every time he selects a cannon the power of the ship will be recalculated
+     * @param cannoni
      * @return
      */
-    public Integer firePower() {
-        // TODO implement here
-        return null;
+    public float firePower(Set<Component> cannoni) {
+        float power  = singleCannonsPower;
+        for(Component cannon : cannoni){
+            power += cannon.getPower();
+        }
+        return power;
     }
 
-    /**
+    /**Function that determines the total engine power of the ship that the user wants to activate
+     * @param doubleEnginesActivated: the number of double engines the user wants to activate => the number of battery cells consumed
      * @return
      */
-    public Integer enginePower() {
-        // TODO implement here
-        return null;
+    public Integer enginePower(Integer doubleEnginesActivated) {
+        return doubleEnginesActivated * 2 + singleEngines;
     }
 
     /**
      * @return
      */
     public Integer batteriesAvailable() {
-        // TODO implement here
         return null;
     }
 
