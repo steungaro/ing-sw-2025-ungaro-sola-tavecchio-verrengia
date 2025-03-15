504023cd4be015c281b11d9588626cbf9ff99aac
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-03-15 23:16:00 +0100

    New controller UML and classes

---
+++ src/main/java/it/polimi/ingsw/gc20/model/lobby/Lobby.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.gc20.model.lobby;
+
+import GameController;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Lobby {
+
+    /**
+     * Default constructor
+     */
+    public Lobby() {
+    }
+
+    /**
+     * 
+     */
+    private List<String> users;
+
+    /**
+     * 
+     */
+    private String id;
+
+    /**
+     * 
+     */
+    private int maxPlayers;
+
+    /**
+     * 
+     */
+    private String ownerUsername;
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * @param
+     * @return
+     */
+    public void removePlayer(String user) {
+        // TODO implement here
+    }
+
+    /**
+     * @param String 
+     * @return
+     */
+    public void addPlayer(String user) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public GameController createGameController() {
+        // TODO implement here
+        return null;
+    }
+
+}
