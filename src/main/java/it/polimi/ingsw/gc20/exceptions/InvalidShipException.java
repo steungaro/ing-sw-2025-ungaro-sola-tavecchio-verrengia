946d0e9f050e5d35220a7a8a3fe84b01fef356ec
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-03-18 11:16:48 +0100

    new fire implementation

---
+++ src/main/java/it/polimi/ingsw/gc20/exceptions/InvalidShipException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.gc20.exceptions;
+
+public class InvalidShipException extends RuntimeException {
+    public InvalidShipException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/gc20/model/cards/FireType.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.gc20.model.cards;
+
+public enum FireType {
+    LIGHT_METEOR,
+    HEAVY_METEOR,
+    LIGHT_FIRE,
+    HEAVY_FIRE
+}
