174344ea3fa54a24c2dac6ddf6b103e34d10fe33
Mic-Tave <michele1.tavecchio@mail.polimi.it>
2025-03-21 11:35:19 +0100

    updateGameID
    change gameid from integer to string

--- src/main/java/it/polimi/ingsw/gc20/model/gamesets/GameModel.java
+++ src/main/java/it/polimi/ingsw/gc20/model/gamesets/GameModel.java
@@ -138,7 +138,7 @@ public class GameModel {
      * @param usernames       list of the players' username
      * @param gameID          id of the game
      */
-    public void startGame(int level, List<String> usernames, int gameID) {
+    public void startGame(int level, List<String> usernames, String gameID) {
         Game game = new Game();
         game.setID(gameID);
         Pile pile = new Pile();
