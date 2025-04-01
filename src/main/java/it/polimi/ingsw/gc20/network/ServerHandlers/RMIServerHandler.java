1687f5781f46096e877ee5ced9c01e422657dd5b
Mic-Tave <michele1.tavecchio@mail.polimi.it>
2025-04-01 10:59:59 +0200

    network to redo

--- src/main/java/it/polimi/ingsw/gc20/network/ServerHandlers/RMIServerHandler.java
+++ src/main/java/it/polimi/ingsw/gc20/network/ServerHandlers/RMIServerHandler.java
@@ -10,12 +10,14 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class RMIServerHandler {
-    private final int port = 1099;
-    boolean registryCreated = false;
-    private int usedPort = 0;
+public class RMIServerHandler extends ServerHandler {
+    private final String ip;
+    private final int port;
 
-
+    public RMIServerHandler(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
     /**
      *  Method to create a registry on a specific port
      * @param port the port on which the registry will be created
--- src/main/java/it/polimi/ingsw/gc20/network/ServerHandlers/ServerHandler.java
+++ src/main/java/it/polimi/ingsw/gc20/network/ServerHandlers/ServerHandler.java
@@ -2,5 +2,5 @@ package it.polimi.ingsw.gc20.network.ServerHandlers;
 
 public abstract class ServerHandler {
 
-    public abstract void connect () t
+    public abstract void connect ();
 }
