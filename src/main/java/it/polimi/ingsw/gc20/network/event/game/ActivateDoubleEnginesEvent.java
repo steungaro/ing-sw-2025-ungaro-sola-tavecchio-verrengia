9540ee0e9a810af0ca2e5e6e78fde25ea70158bd
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-04-18 23:57:46 +0200

    boh

--- src/main/java/it/polimi/ingsw/gc20/network/event/game/ActivateDoubleEnginesEvent.java
+++ src/main/java/it/polimi/ingsw/gc20/network/event/game/ActivateDoubleEnginesEvent.java
@@ -1,21 +1,21 @@
 package it.polimi.ingsw.gc20.network.event.game;
 
-import it.polimi.ingsw.gc20.network.event.Message;
+import it.polimi.ingsw.gc20.network.event.Event;
 import org.javatuples.Pair;
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * Message that is generated when a player activates the double engine
+ * Event that is generated when a player activates the double engine
+ * @apiNote username of the player, list of tuples of the engine and battery components the engines are in the first position while the batteries are in the second
  */
 public record ActivateDoubleEnginesEvent(
         String username,
-        List<Pair<Integer, Integer>> enginesCoordinates,
-        List<Pair<Integer, Integer>> batteriesCoordinates
-) implements Message, Serializable {
+        List<Pair<Integer, Integer>> componentIDs
+) implements Event, Serializable {
 
     @Override
     public String toString() {
-        return "ActivateDoubleCannonsEvent {username = " + username + "engines = " + enginesCoordinates.toString() + " batteries = " + batteriesCoordinates.toString() + "}";
+        return "ActivateDoubleEnginesEvent {username = " + username +  componentIDs.toString() + "}";
     }
 }
