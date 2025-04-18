52454c9c1977ce08df225c805cfe1c4cb3033a66
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-04-18 23:57:46 +0200

    boh

---
+++ src/main/java/it/polimi/ingsw/gc20/network/event/Event.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.gc20.network.event;
+
+import java.io.Serializable;
+
+public interface Event extends Serializable {
+}
--- src/main/java/it/polimi/ingsw/gc20/network/event/game/ActivateDoubleCannonsEvent.java
+++ src/main/java/it/polimi/ingsw/gc20/network/event/game/ActivateDoubleCannonsEvent.java
@@ -1,21 +1,21 @@
 package it.polimi.ingsw.gc20.network.event.game;
 
-import it.polimi.ingsw.gc20.network.event.Message;
+import it.polimi.ingsw.gc20.network.event.Event;
 import org.javatuples.Pair;
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * Message that is generated when a player activates the double cannons
+ * Event that is generated when a player activates the double cannons
+ * @apiNote username of the player, list of tuples of the cannon and battery components the cannons are in the first position while the batteries are in the second
  */
 public record ActivateDoubleCannonsEvent(
         String username,
-        List<Pair<Integer, Integer>> cannonsCoordinates,
-        List<Pair<Integer, Integer>> batteriesCoordinates
-) implements Message, Serializable {
+        List<Pair<Integer, Integer>> coordinates
+) implements Event, Serializable {
 
     @Override
     public String toString() {
-        return "ActivateDoubleCannonsEvent {username = " + username + "cannons = " + cannonsCoordinates.toString() + " batteries = " + batteriesCoordinates.toString() + "}";
+        return "ActivateDoubleCannonsEvent {username = " + username +  componentIDs.toString() + "}";
     }
 }
