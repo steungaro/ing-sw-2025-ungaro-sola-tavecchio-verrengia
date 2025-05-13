973c4f997347821a5b18fae4aebdd0ab3c880dd5
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-13 16:11:13 +0200

    Add and update rendering logic for Adventure Card views
    Enhanced the adventure card classes to include detailed rendering logic for various card types, using new attributes and methods to ensure proper display of card elements. Key changes include added support for dynamic rewards, projectile details, and card-specific designs across all card view types.

--- src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/adventureCards/ViewSlavers.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/adventureCards/ViewSlavers.java
@@ -10,14 +10,15 @@ public class ViewSlavers extends ViewAdvetnureCard {
     public String toString() {
         return
                         up() + "\n" +
-                        lateral() + "  Slavers           " + lateral() + "\n" +
+                        lateral() + "  Slavers             " + lateral() + "\n" +
                         lateral() + EMPTY_ROW + lateral() + "\n" +
-                        lateral() + "  FirePower: " + firePower + "      " + lateral() + "\n" +
-                        lateral() + "  LostCrew: " + lostCrew + "       " + lateral() + "\n" +
+                        lateral() + "  FirePower: " + firePower + "        " + lateral() + "\n" +
+                        lateral() + "  LostCrew: " + lostCrew + "         " + lateral() + "\n" +
                         lateral() + EMPTY_ROW + lateral() + "\n" +
-                        lateral() + "  Credits: " + credits + "$       " + lateral() + "\n" +
-                        lateral() + "  LostDays: " + lostDays + "       " + lateral() + "\n" +
-                        down();
+                        lateral() + "  Credits: " + credits + "$         " + lateral() + "\n" +
+                        lateral() + "  LostDays: " + lostDays + "         " + lateral() + "\n" +
+                                lateral() + EMPTY_ROW + lateral() + "\n" +
+                                down();
     }
 
 }
--- src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/adventureCards/ViewSmugglers.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/adventureCards/ViewSmugglers.java
@@ -3,50 +3,57 @@ package it.polimi.ingsw.gc20.client.view.common.localmodel.adventureCards;
 public class ViewSmugglers extends ViewAdvetnureCard {
     int firePower;
     int lostCargo;
-    int redCargo;
-    int yellowCargo;
-    int greenCargo;
-    int blueCargo;
+    int redCargo = 0;
+    int yellowCargo = 0;
+    int greenCargo = 0;
+    int blueCargo = 0;
+    int lostDays;
 
     @Override
     public String toString() {
         return
                 up() + "\n" +
-                        lateral() + "  Smugglers         " + lateral() + "\n" +
+                        lateral() + "  Smugglers           " + lateral() + "\n" +
                         lateral() + EMPTY_ROW + lateral() + "\n" +
-                        lateral() + "  FirePower: " + firePower + "      " + lateral() + "\n" +
-                        lateral() + "  LostCargo: " + lostCargo + "      " + lateral() + "\n" +
+                        lateral() + "  FirePower: " + firePower + "        " + lateral() + "\n" +
+                        lateral() + "  LostCargo: " + lostCargo + "        " + lateral() + "\n" +
                         lateral() + EMPTY_ROW + lateral() + "\n" +
-                        lateral() + "  Reward: " +   "$        " + lateral() + "\n" +
-                        lateral() + "  LostDays: " +  "       " + lateral() + "\n" +
+                        lateral() + "  Reward: " + reward() +  spaces(12-rewardSize()) + lateral() + "\n" +
+                        lateral() + "  LostDays: " + lostDays + "         " + lateral() + "\n" +
+                        lateral() + EMPTY_ROW + lateral() + "\n" +
                         down();
     }
 
-    private String rewars(){
+    private String reward(){
         String result = "";
+        int j = 0;
         for(int i = 0; i < redCargo; i++){
             if(i==0)
                 result += "R";
             else
                 result += " R";
+            j++;
         }
         for(int i = 0; i < yellowCargo; i++){
-            if(i==0)
+            if(j==0)
                 result += "Y";
             else
                 result += " Y";
+            j++;
         }
         for(int i = 0; i < greenCargo; i++){
-            if(i==0)
+            if(j==0)
                 result += "G";
             else
                 result += " G";
+            j++;
         }
         for(int i = 0; i < blueCargo; i++){
-            if(i==0)
+            if(j==0)
                 result += "B";
             else
                 result += " B";
+            j++;
         }
         return result;
     }
@@ -61,8 +68,6 @@ public class ViewSmugglers extends ViewAdvetnureCard {
             result++;
         if(blueCargo > 0)
             result++;
-
-
 
         return (result-1)*2 + 1;
     }
