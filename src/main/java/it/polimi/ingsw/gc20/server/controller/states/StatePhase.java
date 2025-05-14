757d8f17478212b8112e09efbaefb3986969a182
Mic-Tave <michele1.tavecchio@mail.polimi.it>
2025-05-14 12:11:09 +0200

    Enhance UpdateShipMessage to include booked components and component in hand for improved ship state management

--- src/main/java/it/polimi/ingsw/gc20/server/controller/states/StatePhase.java
+++ src/main/java/it/polimi/ingsw/gc20/server/controller/states/StatePhase.java
@@ -1,4 +1,32 @@
 package it.polimi.ingsw.gc20.server.controller.states;
 
 public enum StatePhase {
-}
+        CANNONS_PHASE("Select cannons and batteries for this phase"),
+        ACCEPT_PHASE("Accept or discard the card"),
+        LOSE_CREW_PHASE("Select the crew to lose"),
+        AUTOMATIC_ACTION("Automatic action will be performed, no action needed"),
+        REMOVE_CARGO("select the cargo to remove, if there are not enough cargoes, you will be prompted to remove batteries"),
+        ADD_CARGO("add cargo to the ship, you can reorganize your cargo"),
+        GET_HIT("your ship will be shot"),
+        LAND_ON_PLANET("Select the planet to land on"),
+        ENGINES_PHASE("Select engines and batteries for this phase"),
+        SELECT_SHIELD("Select the shield to use"),
+        SELECT_BATTERIES("Select the batteries to use"),
+        TAKE_COMPONENT("can select the component you want to take, or a deck to view, or turn the hourglass"),
+        PLACE_COMPONENT("can place the component you took in the previous phase");
+
+        private final String description;
+
+        StatePhase(String description) {
+            this.description = description;
+        }
+
+        /**
+         * Restituisce una descrizione di ciò che il giocatore deve fare durante questa fase
+         * @return stringa di istruzioni per questa fase
+         */
+        public String getDescription() {
+            return description;
+        }
+    }
+
