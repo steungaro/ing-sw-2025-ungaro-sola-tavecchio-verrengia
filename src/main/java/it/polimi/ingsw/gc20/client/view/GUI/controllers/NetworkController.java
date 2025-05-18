3db4340d68565ecd7e0a41fced92b585ce75e702
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-18 11:42:40 +0200

    Add GUI components for welcome, login, main menu, and lobby management

--- src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/NetworkController.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/NetworkController.java
@@ -1,9 +1,12 @@
 package it.polimi.ingsw.gc20.client.view.GUI.controllers;
 
+import it.polimi.ingsw.gc20.client.view.common.View;
 import it.polimi.ingsw.gc20.client.view.GUI.GUIView;
-import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import javafx.fxml.FXML;
 import it.polimi.ingsw.gc20.client.network.common.Client;
+import it.polimi.ingsw.gc20.client.view.common.View;
+import it.polimi.ingsw.gc20.client.view.GUI.GUIView;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
@@ -38,14 +41,17 @@ public class NetworkController {
 
     @FXML
     public void initialize() {
-        guiView = (GUIView) ClientGameModel.getInstance();
+        guiView = (GUIView) View.getInstance();
 
+        // Inizializza il gruppo di toggle per i radio button
         connectionTypeGroup = new ToggleGroup();
         socketRadioButton.setToggleGroup(connectionTypeGroup);
         rmiRadioButton.setToggleGroup(connectionTypeGroup);
 
+        // Seleziona Socket come default
         socketRadioButton.setSelected(true);
 
+        // Valori predefiniti
         ipAddressField.setText("localhost");
         portField.setText("502");
 
