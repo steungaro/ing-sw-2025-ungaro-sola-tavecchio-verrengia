57d9435d9c7ebea0f0d9301d885dfd0550364e92
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-18 11:42:40 +0200

    Add GUI components for welcome, login, main menu, and lobby management

--- src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/LoginController.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/LoginController.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.gc20.client.view.GUI.controllers;
 
+import it.polimi.ingsw.gc20.client.view.common.View;
 import it.polimi.ingsw.gc20.client.view.GUI.GUIView;
-import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -18,7 +18,7 @@ public class LoginController {
 
     @FXML
     public void initialize() {
-        guiView = (GUIView) ClientGameModel.getInstance();
+        guiView = (GUIView) View.getInstance();
 
         loginButton.setOnAction(event -> handleLogin());
     }
@@ -31,8 +31,7 @@ public class LoginController {
             return;
         }
 
-        ClientGameModel.getInstance().setUsername(username);
-        ClientGameModel.getInstance().login();
-        guiView.showScene("mainMenu");
+        // Passa al MainMenu
+        guiView.showMainMenuScene(username);
     }
 }
