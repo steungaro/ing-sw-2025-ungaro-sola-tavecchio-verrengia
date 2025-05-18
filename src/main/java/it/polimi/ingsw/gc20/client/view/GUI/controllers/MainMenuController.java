4319048e975fcd042d04683f0bc844073b576f9c
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-18 11:42:40 +0200

    Add GUI components for welcome, login, main menu, and lobby management

---
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/MainMenuController.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.gc20.client.view.GUI.controllers;
+
+import it.polimi.ingsw.gc20.client.view.common.ClientController;
+import it.polimi.ingsw.gc20.client.view.common.View;
+import it.polimi.ingsw.gc20.client.view.GUI.GUIView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class MainMenuController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Button viewLobbiesButton;
+
+    @FXML
+    private Button createLobbyButton;
+
+    @FXML
+    private Button logoutButton;
+
+    private GUIView guiView;
+    private String username;
+    private ClientController clientController;
+
+    @FXML
+    public void initialize() {
+        guiView = (GUIView) View.getInstance();
+
+
+
+        createLobbyButton.setOnAction(event -> handleCreateLobby());
+        viewLobbiesButton.setOnAction( event -> handleViewLobbies());
+        logoutButton.setOnAction(event -> handleLogout());
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        welcomeLabel.setText("Benvenuto, " + username + "!");
+    }
+
+    private void handleCreateLobby() {
+        guiView.showCreateLobbyScene();
+    }
+    private void handleViewLobbies() {
+        guiView.showLobbyListScene();
+    }
+
+    public void setClientController(ClientController clientController) {
+        this.clientController = clientController;
+    }
+
+    private void handleLogout() {
+        guiView.showNetworkScene();
+    }
+}
