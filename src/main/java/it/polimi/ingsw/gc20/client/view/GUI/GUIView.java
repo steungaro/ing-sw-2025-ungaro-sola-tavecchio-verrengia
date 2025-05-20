46cd1287ad379383c6c7538380619a2b0893c57e
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-20 13:53:25 +0200

    Refactor GUI navigation logic and cleanup unused methods.
    Replaced redundant scene-specific methods with a generic `showScene` method, simplifying FXML navigation. Removed unused or redundant code in `GUIView`, enhancing maintainability and reducing clutter. Updated controllers to align with the new navigation logic.

--- src/main/java/it/polimi/ingsw/gc20/client/view/GUI/GUIView.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/GUIView.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.gc20.client.view.GUI;
 
-import it.polimi.ingsw.gc20.client.view.common.View;
+import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import it.polimi.ingsw.gc20.common.message_protocol.toserver.Message;
 import it.polimi.ingsw.gc20.client.network.NetworkManager;
 import it.polimi.ingsw.gc20.client.view.GUI.controllers.NetworkController;
@@ -13,32 +13,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jline.terminal.impl.CursorSupport;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GUIView extends View {
+public class GUIView extends ClientGameModel {
 
     private static Stage primaryStage;
-    private MenuStateForGui currentState;
-
-    public enum MenuStateForGui {
-        WELCOME,
-        NETWORK_CONFIG,
-        LOGIN,
-        MAIN_MENU,
-        LOBBY_LIST,
-        CREATE_LOBBY,
-        IN_LOBBY,
-        GAME_SCREEN,
-        BUILDING_PHASE,
-        VALIDATION_PHASE,
-        PLANET_PHASE
-    }
-
-    private Map<MenuStateForGui, Object> controllers = new HashMap<>();
 
     public GUIView() {
         super();
@@ -46,55 +30,13 @@ public class GUIView extends View {
 
     public void initGUI(Stage stage) {
         primaryStage = stage;
-        changeState(MenuStateForGui.WELCOME);
+        showScene("welcome");
     }
 
-    public void changeState(MenuStateForGui newState, Object... params) {
-        this.currentState = newState;
-
-        switch(newState) {
-            case WELCOME:
-                showWelcomeScene();
-                break;
-            case NETWORK_CONFIG:
-                showNetworkScene();
-                break;
-            case LOGIN:
-                showLoginScene();
-                break;
-            case MAIN_MENU:
-                showMainMenuScene((String)params[0]);
-                break;
-            case LOBBY_LIST:
-                showLobbyListScene();
-                break;
-            case CREATE_LOBBY:
-                showCreateLobbyScene();
-                break;
-            case IN_LOBBY:
-                showInLobbyScene((Lobby)params[0]);
-                break;
-            // TODO: Implement other states
-        }
-    }
-
-
-    private void showWelcomeScene() {
+    public void showScene(String fileName){
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcome.fxml"));
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root, 600, 400);
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showNetworkScene() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/network.fxml"));
+            String path = "/fxml/" + fileName + ".fxml";
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
             Parent root = loader.load();
 
             Scene scene = new Scene(root, 600, 400);
@@ -130,80 +72,18 @@ public class GUIView extends View {
         }
 
         if (client != null) {
-            showLoginScene();
+            showScene("login");
         }
     }
 
-    public void showMainMenuScene(String username) {
-        client.login(username);
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainMenu.fxml"));
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root, 600, 400);
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showLoginScene() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root, 600, 400);
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showCreateLobbyScene() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/createLobby.fxml")); // Check if necessary
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root, 600, 400);
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showLobbyListScene() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/lobbyList.fxml"));
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root, 600, 400);
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void updateView(Message message) throws RemoteException {
-        super.updateView(message);
-        Platform.runLater(() -> {
-            // TODO: Implementare l'aggiornamento della view
-        });
-    }
 
     @Override
     public void notifyDisconnection() throws RemoteException {
         // TODO: Implementare la notifica di disconnessione
-        Platform.runLater(() -> {
-            // Codice per gestire la disconnessione nell'interfaccia grafica
-        });
     }
 
     public void login(String username, String server, int port) {
-        // TODO: Implementare il login
+        client.login(username);
+        this.username = username;
     }
 }
