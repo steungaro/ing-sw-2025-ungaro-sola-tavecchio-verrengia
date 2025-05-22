b3a5359e99f69dd19240ea76a61e7b986744e031
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-22 10:24:31 +0200

    Add initial board and game controller UI components

---
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/Board0Controller.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.gc20.client.view.GUI.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class Board0Controller {
+
+    @FXML
+    private ImageView boardImageView;
+
+    public void initialize() {
+        // Se necessario, qui puoi aggiungere logica di inizializzazione
+    }
+}
