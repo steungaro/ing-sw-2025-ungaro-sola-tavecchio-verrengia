58f2ae756f1a5d3e46f35894e951ca74556376b4
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-24 19:39:19 +0200

    ClietGameModel fix

--- src/main/java/it/polimi/ingsw/gc20/client/view/TUI/TUI.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/TUI/TUI.java
@@ -5,85 +5,56 @@ import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.adventureCards.ViewAdventureCard;
 import it.polimi.ingsw.gc20.server.model.cards.FireType;
 import it.polimi.ingsw.gc20.server.model.gamesets.CargoColor;
-import org.jline.reader.LineReader;
 import it.polimi.ingsw.gc20.server.model.cards.Planet;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.terminal.Attributes;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.InfoCmp;
-
 import java.io.IOException;
-import java.io.Reader;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class TUI extends ClientGameModel {
     private static final Logger LOGGER = Logger.getLogger(TUI.class.getName());
-
-    private static Terminal terminal;
-    private static LineReader reader;
     private MenuState currentState;
 
-    public TUI() {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public TUI() throws RemoteException{
+        super();
         LOGGER.info("TUI created");
     }
 
     @Override
     public void notifyDisconnection() throws RemoteException {
-        terminal.writer().println("\033[31mDisconnected from server.\033[0m");
-        terminal.flush();
+        System.out.println("\033[31mDisconnected from server.\033[0m");
     }
 
     public void init() {
+        clearConsole();
         System.out.println("Welcome to Galaxy Trucker!");
-
-        try {
-            terminal = TerminalBuilder.builder().system(true).build();
-            reader = LineReaderBuilder.builder().terminal(terminal).build();
-        } catch (Exception e) {
-            LOGGER.warning("Error while initializing terminal: " + e.getMessage());
-        }
-
         printLogo();
     }
 
-    public static void clearConsole(Terminal terminal) {
-        terminal.puts(InfoCmp.Capability.clear_screen);
-        terminal.flush();
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println();
+        System.out.println();
     }
 
     private void printLogo() {
-        terminal.writer().println(" ██████╗  █████╗ ██╗      █████╗ ██╗  ██╗██╗   ██╗    ████████╗██████╗ ██╗   ██╗ ██████╗██╗  ██╗███████╗██████╗ ");
-        terminal.writer().println("██╔════╝ ██╔══██╗██║     ██╔══██╗╚██╗██╔╝╚██╗ ██╔╝    ╚══██╔══╝██╔══██╗██║   ██║██╔════╝██║ ██╔╝██╔════╝██╔══██╗");
-        terminal.writer().println("██║  ███╗███████║██║     ███████║ ╚███╔╝  ╚████╔╝        ██║   ██████╔╝██║   ██║██║     █████╔╝ █████╗  ██████╔╝");
-        terminal.writer().println("██║   ██║██╔══██║██║     ██╔══██║ ██╔██╗   ╚██╔╝         ██║   ██╔══██╗██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗");
-        terminal.writer().println("╚██████╔╝██║  ██║███████╗██║  ██║██╔╝ ██╗   ██║          ██║   ██║  ██║╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║");
-        terminal.writer().println(" ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝");
-        terminal.flush();
-        terminal.writer().println("Press any key to continue...");
-        terminal.flush();
-        try {
-            // Hide cursor
-            terminal.puts(InfoCmp.Capability.cursor_invisible);
-            terminal.reader().read();
-        } catch (Exception e) {
-            LOGGER.warning("Error while waiting for user input: " + e.getMessage());
-        }
+        System.out.println(" ██████╗  █████╗ ██╗      █████╗ ██╗  ██╗██╗   ██╗    ████████╗██████╗ ██╗   ██╗ ██████╗██╗  ██╗███████╗██████╗ ");
+        System.out.println("██╔════╝ ██╔══██╗██║     ██╔══██╗╚██╗██╔╝╚██╗ ██╔╝    ╚══██╔══╝██╔══██╗██║   ██║██╔════╝██║ ██╔╝██╔════╝██╔══██╗");
+        System.out.println("██║  ███╗███████║██║     ███████║ ╚███╔╝  ╚████╔╝        ██║   ██████╔╝██║   ██║██║     █████╔╝ █████╗  ██████╔╝");
+        System.out.println("██║   ██║██╔══██║██║     ██╔══██║ ██╔██╗   ╚██╔╝         ██║   ██╔══██╗██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗");
+        System.out.println("╚██████╔╝██║  ██║███████╗██║  ██║██╔╝ ██╗   ██║          ██║   ██║  ██║╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║");
+        System.out.println(" ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝");
     }
 
     public void shutdown() {
-        terminal.writer().println("Application shutting down.");
-        terminal.flush();
+        System.out.println("Application shutting down.");
         client.stop();
-        try {
-            terminal.close();
-        } catch (Exception e) {
-            LOGGER.warning("Error while closing terminal: " + e.getMessage());
-        }
         System.exit(0);
     }
 
@@ -95,72 +66,47 @@ public class TUI extends ClientGameModel {
         }
     }
 
-    public Terminal getTerminal() {
-        return terminal;
-    }
-
-    public static void hideCursor(Terminal terminal) {
-        terminal.puts(InfoCmp.Capability.cursor_invisible);
-        Attributes attributes = terminal.getAttributes();
-        attributes.setLocalFlag(Attributes.LocalFlag.ECHO, false);
-        terminal.setAttributes(attributes);
-    }
-
-    public static void showCursor(Terminal terminal) {
-        terminal.puts(InfoCmp.Capability.cursor_visible);
-        Attributes attributes = terminal.getAttributes();
-        attributes.setLocalFlag(Attributes.LocalFlag.ECHO, true);
-        terminal.setAttributes(attributes);
-    }
-
+    @SuppressWarnings("BusyWait")
     public void initNetwork() {
         String[] networkTypes = {"RMI", "Socket"};
-        int selectedIndex = 0; // To track the currently highlighted menu option
+        int selectedIndex; // To track the currently highlighted menu option
 
         try {
 
             do {
-                clearConsole(terminal);
-                terminal.writer().println("Select network type:");
+                System.out.println("Select network type:");
 
                 for (int i = 0; i < networkTypes.length; i++) {
-                    terminal.writer().println((i + 1) + ". " + networkTypes[i]);
+                    System.out.println((i + 1) + ". " + networkTypes[i]);
                 }
-                terminal.flush();
 
-                // Hide cursor
-                hideCursor(terminal);
-
-                Reader treader = terminal.reader();
+                System.out.print(" > ");
 
                 // Read user input
+                label:
                 do {
-                    int key = treader.read();
-                    if (key == 'q') {
-                        System.exit(0);
-                    } else if (key == '1') {
-                        break;
-                    } else if (key == '2') {
-                        selectedIndex = 1;
-                        break;
+                    String input = scanner.nextLine().trim();
+                    switch (input) {
+                        case "q":
+                            System.exit(0);
+                        case "1":
+                            selectedIndex = 0;
+                            break label;
+                        case "2":
+                            selectedIndex = 1;
+                            break label;
                     }
                 } while (true);
 
-                // Reset terminal attributes
-                showCursor(terminal);
-
-                // Final selected network type
-                String selectedNetworkType = networkTypes[selectedIndex];
-
-                // Show cursor
-                terminal.puts(InfoCmp.Capability.cursor_visible);
-
                 // Ask for address and port
-                String address = reader.readLine("Insert server address (leave blank for default):\n > ").trim();
-                String port = reader.readLine("Insert server port (leave blank for default):\n > ").trim();
+                System.out.print("Insert server address (leave blank for default):\n > ");
+                String address = scanner.nextLine().trim();
+                System.out.print("Insert server port (leave blank for default):\n > ");
+                String port = scanner.nextLine().trim();
 
-                terminal.writer().println("Trying connection...");
-                terminal.flush();
+                clearConsole();
+
+                System.out.println("\033[33mTrying " + networkTypes[selectedIndex] + " connection...\033[0m");
 
                 try {
                     Thread.sleep(1000);
@@ -170,21 +116,25 @@ public class TUI extends ClientGameModel {
 
                 // Establish the connection based on user input
                 if (address.isBlank()) {
-                    client = NetworkManager.initConnection(selectedNetworkType);
+                    client = NetworkManager.initConnection(networkTypes[selectedIndex]);
                 } else if (port.isBlank()) {
-                    client = NetworkManager.initConnection(selectedNetworkType, address);
+                    client = NetworkManager.initConnection(networkTypes[selectedIndex], address);
                 } else {
-                    client = NetworkManager.initConnection(selectedNetworkType, address, Integer.parseInt(port));
+                    client = NetworkManager.initConnection(networkTypes[selectedIndex], address, Integer.parseInt(port));
                 }
 
-                if (client == null || !client.isConnected()) {
-                    // Hide cursor
-                    hideCursor(terminal);
-                    terminal.writer().println("\033[31mConnection failed. Press any key to try again, type [q] to exit.\033[0m");
-                    terminal.flush();
+                clearConsole();
 
-                    int retry = treader.read();
-                    if (retry == 'q') {
+                if (client == null || !client.isConnected()) {
+                    System.out.println("\033[31mConnection failed. Type any key to try again, type [q] to exit.\033[0m");
+                    System.out.print(" > ");
+
+                    if (client != null) {
+                        client.stop();
+                    }
+
+                    String retry = scanner.nextLine().trim();
+                    if (retry.equals("q")) {
                         System.exit(0);
                     }
                     client = null;
@@ -193,21 +143,47 @@ public class TUI extends ClientGameModel {
 
                 // Connection established
 
-                terminal.writer().println("\033[32mConnection established with server at " + client.getAddress() + ":" + client.getPort() + "\033[0m");
-                terminal.writer().println("Use [q] to quit the application at any time (works in every menu).");
-                terminal.flush();
+                System.out.println("\033[32mConnection established with server at " + client.getAddress() + ":" + client.getPort() + "\033[0m");
+                System.out.println("Use [q] to quit the application at any time (works in every menu).");
 
-                clearConsole(terminal);
         } catch (Exception e) {
-            terminal.writer().println("\033[31mAn error occurred: " + e.getMessage() + "\033[0m");
-            terminal.flush();
+            System.out.println("\033[31mAn error occurred: " + e.getMessage() + "\033[0m");
         }
     }
 
-    @Override
+    public static void viewOptionsMenu() {
+        clearConsole();
+        System.out.println("\u001B[1mViewing options:\u001B[0m");
+        System.out.println("1. View game board");
+        System.out.println("2. View a player's ship");
+        System.out.println("3. View uncovered components");
+        System.out.println("4. View current card");
+        System.out.println("b. Back to the main menu");
+        System.out.print(" > ");
+
+        String input = scanner.nextLine().trim();
+        switch (input) {
+            case "1" -> ClientGameModel.getInstance().printBoard();
+            case "2" -> {
+                System.out.print("Insert the username of the player you want to view:\n > ");
+                String player = scanner.nextLine().trim();
+                if (ClientGameModel.getInstance().getShip(player) != null) {
+                    ClientGameModel.getInstance().printShip(player);
+                } else {
+                    System.out.println("\033[31mPlayer not found.\033[0m");
+                }
+            }
+            case "3" -> ClientGameModel.getInstance().printViewedPile();
+            case "4" -> ClientGameModel.getInstance().printCurrentCard();
+            case "b" -> {
+            }
+            default -> System.out.println("\033[31mInvalid option. Back to the main menu.\033[0m");
+        }
+    }
+
     public void login() {
-        clearConsole(terminal);
-        String inputUsername = reader.readLine("Insert username:\n > ").trim();
+        System.out.print("Insert username (or type [q] to quit):\n > ");
+        String inputUsername = scanner.nextLine().trim();
 
         if (inputUsername.equalsIgnoreCase("q")) {
             shutdown();
@@ -215,9 +191,9 @@ public class TUI extends ClientGameModel {
         }
 
         if (inputUsername.isBlank() || inputUsername.equals("__BROADCAST__")) {
-            terminal.writer().println("\033[31mUsername not valid. Please try again.\033[0m");
-            terminal.flush();
+            System.out.println("\033[31mUsername not valid. Please try again.\033[0m");
         } else {
+            System.out.println("\033[33mLogging in as: " + inputUsername + "\033[0m");
             client.login(inputUsername);
             this.username = inputUsername;
         }
@@ -228,9 +204,7 @@ public class TUI extends ClientGameModel {
         boolean input = false;
         while (!input) {
             try {
-                LOGGER.info("Displaying menu: " + currentState.getClass().getSimpleName());
                 currentState.displayMenu();
-                LOGGER.info("Menu displayed: " + currentState.getClass().getSimpleName());
                 input = currentState.handleInput();
             } catch (IOException e){
                 LOGGER.warning("Error while handling input: " + e.getMessage());
@@ -238,11 +212,12 @@ public class TUI extends ClientGameModel {
         }
     }
 
-    //Display the menu after we get an error, it does not change the state, simply returns to last menu
+    //Display the menu after we get an error, it does not change the state, simply returns to the last menu
     public void display(String message) {
         boolean input = false;
         while (!input) {
             try {
+                System.out.println(message);
                 currentState.displayMenu();
                 input = currentState.handleInput();
             } catch (IOException e){
@@ -252,57 +227,55 @@ public class TUI extends ClientGameModel {
     }
 
     public void branchMenu(){
-        MenuState menu = new BranchMenu(terminal);
-        display(menu);
+        currentState = new BranchMenu();
+        display(currentState);
     }
 
     public void buildingMenu(List<ViewAdventureCard> adventureCards){
-        MenuState menu = new BuildingMenu(terminal, adventureCards);
-        display(menu);
+        currentState= new BuildingMenu(adventureCards);
+        display(currentState);
     }
 
     public void inLobbyMenu(){
-        MenuState menu = new InLobbyMenu(terminal);
+        MenuState menu = new InLobbyMenu();
         display(menu);
     }
 
     public void cannonsMenu(String message){
-        MenuState menu = new CannonsMenu(terminal, message);
+        MenuState menu = new CannonsMenu(message);
         display(menu);
     }
 
     public void cardAcceptanceMenu(String message){
-        MenuState menu = new CardAcceptanceMenu(terminal, message);
+        MenuState menu = new CardAcceptanceMenu(message);
         display(menu);
     }
 
     public void engineMenu(String message){
-        MenuState menu = new EngineMenu(terminal, message);
+        MenuState menu = new EngineMenu(message);
         display(menu);
     }
 
     public void cargoMenu(String message, int cargoToLose, List<CargoColor> cargoToGain){
         //conversion from list CargoColor to Map<Integer, CargoColor>
         Map<CargoColor, Integer> cargoMap = new HashMap<>();
-        cargoToGain.forEach(cargoColor -> {
-            cargoMap.put(cargoColor, cargoMap.getOrDefault(cargoColor, 0) + 1);
-        });
-        MenuState menu = new CargoMenu(terminal, message, cargoToLose, cargoMap);
+        cargoToGain.forEach(cargoColor -> cargoMap.put(cargoColor, cargoMap.getOrDefault(cargoColor, 0) + 1));
+        MenuState menu = new CargoMenu(message, cargoToLose, cargoMap);
         display(menu);
     }
 
     public void planetMenu(List<Planet> planets){
-        MenuState menu = new PlanetMenu(terminal, planets);
+        MenuState menu = new PlanetMenu(planets);
         display(menu);
     }
 
     public void populateShipMenu(){
-        MenuState menu = new PopulateShipMenu(terminal);
+        MenuState menu = new PopulateShipMenu();
         display(menu);
     }
 
     public void validationMenu(){
-        MenuState menu = new ValidationMenu(terminal);
+        MenuState menu = new ValidationMenu();
         display(menu);
     }
 
@@ -311,20 +284,20 @@ public class TUI extends ClientGameModel {
     }
 
     public void mainMenuState(){
-        MenuState menu = new MainMenuState(terminal);
-        LOGGER.info("Main menu displayed");
+        MenuState menu = new MainMenuState();
         display(menu);
     }
 
     public void takeComponentMenu(){
-        //TODO: implement this method
+        MenuState menu = new BuildingMenu(null);
+        display(menu);
     }
 
     public void shieldsMenu(FireType fireType, int direction, int line){
         String[] directions = {"UP", "RIGHT", "DOWN", "LEFT"};
         String Message = fireType + " from " + directions[direction]  +  " at line " + line;
-        MenuState state = new ShieldsMenu(getTerminal(), Message);
-        // ClientGameModel.display(state);
+        MenuState menu = new ShieldsMenu(Message);
+        display(menu);
     }
 
     public void rollDiceMenu(FireType fireType, int direction){
@@ -332,7 +305,8 @@ public class TUI extends ClientGameModel {
     }
 
     public void cargoMenu(int cargoNum){
-        // TODO: implement this method
+        MenuState menu = new CargoMenu(null, cargoNum, new HashMap<>());
+        display(menu);
     }
 
     public void loseCrewMenu(int crewNum){
@@ -344,7 +318,8 @@ public class TUI extends ClientGameModel {
     }
 
     public void placeComponentMenu(){
-        // TODO: implement this method
+        MenuState menu = new BuildingMenu(null);
+        display(menu);
     }
 
     public void leaderBoardMenu(Map<String, Integer> leaderBoard){
@@ -353,17 +328,15 @@ public class TUI extends ClientGameModel {
 
     @Override
     public void loginSuccessful(String username) {
-        clearConsole(terminal);
-        terminal.writer().println("\033[32mLogged in as: " + username + "\033[0m");
-        terminal.flush();
+        clearConsole();
+        System.out.println("\033[32mLogged in as: " + username + "\033[0m");
         wait(1);
     }
 
     @Override
     public void loginFailed(String username) {
-        clearConsole(terminal);
-        terminal.writer().println("\033[31mLogin failed for user: " + username + "\033[0m");
-        terminal.flush();
+        clearConsole();
+        System.out.println("\033[31mLogin failed for user: " + username + "\033[0m");
         login();
     }
 }
