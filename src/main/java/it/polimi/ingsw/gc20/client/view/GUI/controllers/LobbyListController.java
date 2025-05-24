215c4fa20f580c7febb5569077306882bbf86bf6
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-24 19:56:35 +0200

    GUI startup fix

--- src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/LobbyListController.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/LobbyListController.java
@@ -1,7 +1,8 @@
 package it.polimi.ingsw.gc20.client.view.GUI.controllers;
 
-import it.polimi.ingsw.gc20.client.view.common.ClientController;
-
+import it.polimi.ingsw.gc20.client.network.common.Client;
+import it.polimi.ingsw.gc20.client.view.common.ViewLobby;
+import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,15 +11,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
-import it.polimi.ingsw.gc20.server.model.lobby.Lobby;
-
+import javafx.scene.control.ListCell;
+import java.io.Console;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class LobbyListController {
 
     @FXML
-    private ListView<Lobby> lobbiesListView;
+    private ListView<ViewLobby> lobbiesListView;
     
     @FXML
     private Button joinLobbyButton;
@@ -28,18 +30,27 @@ public class LobbyListController {
     
     @FXML
     private Button backButton;
-    
-    private ClientController clientController;
-    
-    public void setClientController(ClientController clientController) {
-        this.clientController = clientController;
-    }
-    
+
     @FXML
     private void initialize() {
         lobbiesListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        
-        lobbiesListView.setCellFactory(// TODO);
+
+        lobbiesListView.setCellFactory(listView -> new ListCell<ViewLobby>() {
+            @Override
+            protected void updateItem(ViewLobby lobby, boolean empty) {
+                super.updateItem(lobby, empty);
+
+                if (empty || lobby == null) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    // Qui definisci come vuoi visualizzare l'oggetto ViewLobby
+                    setText(lobby.getOwner() + " (Proprietario: " + lobby.getOwner() + ") - " +
+                            "Giocatori: " + lobby.getPlayersList() + "/" + lobby.getMaxPlayers());
+                }
+            }
+        });
+
 
         lobbiesListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
             joinLobbyButton.setDisable(newVal == null);
@@ -49,94 +60,57 @@ public class LobbyListController {
         refreshButton.setOnAction(event -> onRefreshLobbies());
         backButton.setOnAction(event -> onBack());
         
-        // Carica inizialmente le lobby
         loadLobbies();
     }
     
     private void loadLobbies() {
         // Implementazione per caricare le lobby dal server
-        if (clientController != null) {
-            List<Lobby> lobbies = clientController.getAvailableLobbies();
+        if (ClientGameModel.getInstance().getClient() != null) {
+            List<ViewLobby> lobbies = ClientGameModel.getInstance().getLobbyList();
             lobbiesListView.getItems().clear();
-            // lobbiesListView.getItems().addAll(lobbies);
+            lobbiesListView.getItems().addAll(lobbies);
         }
     }
     
     private void onJoinLobby() {
-        Lobby selectedLobby = lobbiesListView.getSelectionModel().getSelectedItem();
+        ViewLobby selectedLobby = lobbiesListView.getSelectionModel().getSelectedItem();
         if (selectedLobby != null) {
             // 1. Invia richiesta al server per entrare nella lobby
-            boolean joinSuccessful = joinLobbyOnServer(selectedLobby);
-            
-            // 2. Se l'ingresso è avvenuto con successo, naviga alla schermata InLobby
-            if (joinSuccessful) {
-                navigateToInLobbyScreen(selectedLobby);
+            joinLobbyOnServer(selectedLobby);
+        }
+    }
+    
+    private void joinLobbyOnServer(ViewLobby lobby) {
+        Client client = ClientGameModel.getInstance().getClient();
+        if (client != null) {
+            try {
+                client.joinLobby(lobby.getID(), lobby.getOwner());
+            } catch (java.rmi.RemoteException e){
+                System.out.println("Errore di connessione al server: " + e.getMessage());
             }
         }
     }
     
-    private boolean joinLobbyOnServer(Lobby lobby) {
-        // Implementa la comunicazione con il server per entrare nella lobby
-        if (clientController != null) {
-            // return clientController.joinLobby(lobby.getId());
-        }
-        
-        // Placeholder per test
-        return true;
-    }
-    
-    private void navigateToInLobbyScreen(Lobby lobby) {
-        try {
-            // Carica l'FXML per la schermata InLobby
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/InLobby.fxml"));
-            Parent inLobbyRoot = loader.load();
-            
-            // Ottieni il controller e inizializzalo con i dati della lobby
-            InLobbyController inLobbyController = loader.getController();
-            inLobbyController.setClientController(clientController);
-            inLobbyController.setupLobby(lobby);
-            
-            // Ottieni lo Stage corrente dalla scena attuale
-            Stage stage = (Stage) joinLobbyButton.getScene().getWindow();
-            
-            // Crea e imposta la nuova scena
-            Scene inLobbyScene = new Scene(inLobbyRoot);
-            stage.setScene(inLobbyScene);
-            stage.show();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Mostra un messaggio di errore all'utente
-            // Ad esempio, usando un Alert
-        }
-    }
-    
     private void onRefreshLobbies() {
-        // Aggiorna l'elenco delle lobby
         loadLobbies();
     }
     
     private void onBack() {
         try {
-            // Carica l'FXML per la schermata del menu principale
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
             Parent mainMenuRoot = loader.load();
             
-            // Ottieni il controller e inizializzalo
             MainMenuController mainMenuController = loader.getController();
-            mainMenuController.setClientController(clientController);
-            
-            // Ottieni lo Stage corrente dalla scena attuale
+            // TODO Disconnect client
+
             Stage stage = (Stage) backButton.getScene().getWindow();
             
-            // Crea e imposta la nuova scena
             Scene mainMenuScene = new Scene(mainMenuRoot);
             stage.setScene(mainMenuScene);
             stage.show();
             
         } catch (IOException e) {
             e.printStackTrace();
-            // Mostra un messaggio di errore all'utente
         }
     }
 }
