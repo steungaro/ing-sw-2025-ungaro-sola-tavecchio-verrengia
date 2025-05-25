dcda7294c063165b94f77a95f84a44add78429b8
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-05-25 15:55:58 +0200

    Enhance in-lobby functionality by adding data initialization and action handlers for buttons

--- src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/InLobbyController.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/GUI/controllers/InLobbyController.java
@@ -1,21 +1,14 @@
-
 package it.polimi.ingsw.gc20.client.view.GUI.controllers;
 
 import it.polimi.ingsw.gc20.client.view.GUI.GUIView;
+import it.polimi.ingsw.gc20.client.view.common.ViewLobby;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import it.polimi.ingsw.gc20.server.model.lobby.Lobby;
-import it.polimi.ingsw.gc20.server.model.player.Player;
 
-import java.io.IOException;
 import java.util.List;
 
 public class InLobbyController {
@@ -44,65 +37,54 @@ public class InLobbyController {
     @FXML
     private Button killLobbyButton;
 
-    private Lobby currentLobby;
+    private ViewLobby currentLobby;
     private boolean isOwner = false;
     private String currentUsername;
     private ClientGameModel clientController = ClientGameModel.getInstance();
 
-    // Riferimento al ClientController per comunicare con il server
-    private ClientController clientController;
-
-    public void setClientController(ClientController clientController) {
-        this.clientController = clientController;
-    }
-
     @FXML
-    private void initialize() {
-        setupLobby(currentLobby, currentUsername);
-        startGameButton.setOnAction(event -> onStartGame());
-        leaveLobbyButton.setOnAction(event -> onLeaveLobby());
-        killLobbyButton.setOnAction(event -> onKillLobby());
+    public void initialize() {
+        // Gli handler verranno collegati tramite gli attributi onAction nel file FXML
+        // Inizializzazione della lobby verrà fatta attraverso initLobbyData
     }
 
-    /**
-     * Configura la schermata della lobby con i dati specifici
-     * @param lobby La lobby in cui si trova il giocatore
-     * @param username Il nome utente del giocatore corrente
-     */
-    public void setupLobby(Lobby lobby, String username) {
-        this.currentLobby = lobby;
-        this.currentUsername = username;
+    public void initLobbyData(ViewLobby lobby, String username) {
+        if (lobby != null) {
+            this.currentLobby = lobby;
+            lobbyTitleLabel.setText("Lobby: " + lobby.getID());
+            currentUsername = username;
 
-        this.isOwner = username.equals(lobby.getOwnerUsername());
+            playerCountLabel.setText(String.format("%d/%d giocatori",
+                    lobby.getPlayersList().size(),
+                    lobby.getMaxPlayers()));
 
-        lobbyTitleLabel.setText("LOBBY DI " + lobby.getOwnerUsername().toUpperCase());
-        updatePlayerCount();
+            updatePlayerList(lobby, username);
 
-        ownerControlsBox.setVisible(isOwner);
-        waitingMessageLabel.setVisible(!isOwner);
-        killLobbyButton.setVisible(isOwner);
+            isOwner = username.equals(lobby.getOwner());
+            ownerControlsBox.setVisible(isOwner);
 
-        if (isOwner) {
             updateStartButtonState();
-            leaveLobbyButton.setText("ESCI DALLA LOBBY");
         } else {
-            leaveLobbyButton.setText("LASCIA LOBBY");
+            lobbyTitleLabel.setText("Errore: Dati della lobby non disponibili");
         }
-
-        loadPlayers();
-        startLobbyUpdateTimer();
-
-        System.out.println("Lobby attuale: " + ClientGameModel.getInstance().getCurrentLobby());
     }
 
-    private void updatePlayerCount() {
-        playerCountLabel.setText(String.format("%d/%d giocatori",
-                currentLobby.getUsers().size(),
-                currentLobby.getMaxPlayers()));
+    private void updatePlayerList(ViewLobby lobby, String username) {
+        playersListView.getItems().clear();
+        for (String player : lobby.getPlayersList()) {
+            String displayName = player;
+            if (player.equals(lobby.getOwner())) {
+                displayName += " (Proprietario)";
+            }
+            if (player.equals(username)) {
+                displayName += " (Tu)";
+            }
+            playersListView.getItems().add(displayName);
+        }
     }
 
     private void updateStartButtonState() {
-        boolean canStart = currentLobby.getUsers().size() >= 2;
+        boolean canStart = currentLobby != null && currentLobby.getPlayersList().size() >= 2;
         startGameButton.setDisable(!canStart);
 
         if (!canStart) {
@@ -114,59 +96,37 @@ public class InLobbyController {
         }
     }
 
-    private void loadPlayers() {
-        playersListView.getItems().clear();
-
-        List<String> players = currentLobby.getUsers();
-
-        for (String player : players) {
-            String displayName = player;
-            if (player.equals(currentLobby.getOwnerUsername())) {
-                displayName += " (Proprietario)";
-            }
-            if (player.equals(currentUsername)) {
-                displayName += " (Tu)";
-            }
-            playersListView.getItems().add(displayName);
-        }
-    }
-
-    private void startLobbyUpdateTimer() {
-        // TODO: Maybe
-    }
-
-    private void onStartGame() {
+    @FXML
+    public void onStartGame() {
         if (!isOwner) return;
 
         try {
             ClientGameModel.getInstance().getClient().startLobby(currentUsername);
+            ((GUIView)ClientGameModel.getInstance()).showScene("game");
         } catch (Exception e) {
             e.printStackTrace();
-            return;
         }
-        ((GUIView)ClientGameModel.getInstance()).showScene("game");
     }
 
-    private void onLeaveLobby() {
-
+    @FXML
+    public void onLeaveLobby() {
         try {
             ClientGameModel.getInstance().getClient().leaveLobby(currentUsername);
+            ((GUIView)ClientGameModel.getInstance()).showScene("login");
         } catch (Exception e) {
             e.printStackTrace();
-            return;
         }
-        ((GUIView)ClientGameModel.getInstance()).showScene("login");
     }
 
-    private void onKillLobby() {
+    @FXML
+    public void onKillLobby() {
         if (!isOwner) return;
 
         try {
             ClientGameModel.getInstance().getClient().killLobby(currentUsername);
+            ((GUIView)ClientGameModel.getInstance()).showScene("login");
         } catch (Exception e) {
             e.printStackTrace();
-            return;
         }
-        ((GUIView)ClientGameModel.getInstance()).showScene("login");
     }
 }
