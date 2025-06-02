433c2acbd8a3624c632ae02c336c267269987f32
Mic-Tave <michele1.tavecchio@mail.polimi.it>
2025-06-02 14:24:45 +0200

    Refactor boundary checks in NormalShip to improve component validation logic

--- src/main/java/it/polimi/ingsw/gc20/server/model/ship/NormalShip.java
+++ src/main/java/it/polimi/ingsw/gc20/server/model/ship/NormalShip.java
@@ -243,7 +243,10 @@ public class NormalShip extends Ship {
                     col++;
                     break;
             }
-            if (row>getRows() || row<0 || col>getCols() || col<0 || table[row][col].getComponent() == null) {
+            if (row>getRows() || row<0 || col>getCols() || col<0) {
+                continue;
+            }
+            if ( table[row][col].getComponent() == null) {
                 continue;
             }
             if (entry.getValue() == ConnectorEnum.ZERO || !(table[row][col].getComponent().isCabin())) {
@@ -287,7 +290,10 @@ public class NormalShip extends Ship {
                     col++;
                     break;
             }
-            if (table[row][col].getComponent() == null) {
+            if (row>getRows() || row<0 || col>getCols() || col<0) {
+                continue;
+            }
+            if ( table[row][col].getComponent() == null) {
                 continue;
             }
 
