4fa5c7d539436e61a6e6e5eab3844d9065e4f79c
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-06-03 14:49:00 +0200

    Enhance ship management and UI updates; add periodic updates for lobby data and improve error messages

--- src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/ClientGameModel.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/ClientGameModel.java
@@ -37,7 +37,7 @@ public abstract class ClientGameModel extends UnicastRemoteObject implements Vie
     protected String username;
     protected Client client;
     private ViewBoard board;
-    private Map<String, ViewShip> ships;
+    protected Map<String, ViewShip> ships;
     protected ViewAdventureCard currentCard;
     private final List<GameModelListener> listeners = new ArrayList<>();
     private ViewComponent componentInHand;
