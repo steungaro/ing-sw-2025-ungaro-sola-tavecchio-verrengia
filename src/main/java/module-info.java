f5836452867d32f01d0c4e562732c36422a8ae0b
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-06-10 00:41:57 +0200

    Add new module exports to module-info.java
    New export statements have been added for server controller states, managers, and message protocol packages, as well as client TUI and GUI components. This enhances module accessibility and allows necessary inter-module communication.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -24,6 +24,12 @@ module it.polimi.ingsw.gc20 {
     exports it.polimi.ingsw.gc20.client.view.common.localmodel.ship;
     exports it.polimi.ingsw.gc20.client.view.common.localmodel.adventureCards;
     exports it.polimi.ingsw.gc20.server.exceptions;
+    exports it.polimi.ingsw.gc20.server.controller.states;
+    exports it.polimi.ingsw.gc20.server.controller.managers;
+    exports it.polimi.ingsw.gc20.common.message_protocol.toclient;
+    exports it.polimi.ingsw.gc20.common.message_protocol.toserver;
+    exports it.polimi.ingsw.gc20.client.view.TUI;
+    exports it.polimi.ingsw.gc20.client.view.GUI.controllers;
     opens it.polimi.ingsw.gc20.server.model.components to com.fasterxml.jackson.databind;
     opens it.polimi.ingsw.gc20.server.model.ship to com.fasterxml.jackson.databind;
     opens it.polimi.ingsw.gc20.server.model.cards to com.fasterxml.jackson.databind;
