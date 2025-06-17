e98b5a3d0d8a50acfa037c0c27391399b1bc3391
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-06-17 10:58:22 +0200

    Update cargo display formatting and replace row logic.
    Improved TUI cargo display alignment by refining row calculations and replacing unused row variables. Enhanced clarity and consistency in visual representation by updating the handling of free space markers.

--- src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/components/ViewCargoHold.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/common/localmodel/components/ViewCargoHold.java
@@ -14,35 +14,30 @@ public class ViewCargoHold extends ViewComponent {
     @Override
     public String toString() {
         String row0;
-        String row1;
         String row2;
 
         row0 = "\u001B[31mR \u001B[0m".repeat(red) + "\u001B[32mG \u001B[0m".repeat(green) + "\u001B[34mB \u001B[0m".repeat(blue) + "\u001B[33mY \u001B[0m".repeat(yellow);
-        row1 = " ".repeat((14 - (2*red + 2*green + 2*blue + 2*yellow))/2) + row0 + " ".repeat((13 - (2*red + 2*yellow + 2*green + 2*blue))/2);
-        row2 = " ".repeat(7 - free) + "F ".repeat(free) + " ".repeat(6 - free);
-
+        row2 = " ".repeat((14 - (2*red + 2*green + 2*blue + 2*yellow + 2*free))/2) + "E ".repeat(free) + row0 + " ".repeat((13 - (2*red + 2*yellow + 2*green + 2*blue + 2*free))/2);
 
         return
                 firstRow() + "\n" +
                 leftCol(0) + "  CargoHold  " + rightCol(0) + "\n" +
-                leftCol(1) + row1 + rightCol(1) + "\n" +
+                leftCol(1) + EMPTY_ROW + rightCol(1) + "\n" +
                 leftCol(2) + row2 + rightCol(2) + "\n" +
                 lastRow();
     }
 
     public String toLine(int i) {
         String row0;
-        String row1;
         String row2;
 
         row0 = "\u001B[31mR \u001B[0m".repeat(red) + "\u001B[32mG \u001B[0m".repeat(green) + "\u001B[34mB \u001B[0m".repeat(blue) + "\u001B[33mY \u001B[0m".repeat(yellow);
-        row1 = " ".repeat((14 - (2*red + 2*green + 2*blue + 2*yellow))/2) + row0 + " ".repeat((13 - (2*red + 2*yellow + 2*green + 2*blue))/2);
-        row2 = " ".repeat(7 - free) + "F ".repeat(free) + " ".repeat(6 - free);
+        row2 = " ".repeat((14 - (2*red + 2*green + 2*blue + 2*yellow + 2*free))/2) + "E ".repeat(free) + row0 + " ".repeat((13 - (2*red + 2*yellow + 2*green + 2*blue + 2*free))/2);
 
         return switch (i) {
             case 0 -> firstRow();
             case 1 -> leftCol(0) + "  CargoHold  " + rightCol(0);
-            case 2 -> leftCol(1) + row1 + rightCol(1);
+            case 2 -> leftCol(1) + EMPTY_ROW + rightCol(1);
             case 3 -> leftCol(2) + row2 + rightCol(2);
             case 4 -> lastRow();
             default -> "";
