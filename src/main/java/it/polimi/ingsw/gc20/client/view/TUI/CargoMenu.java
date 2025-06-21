fa0244119930c36d7703907d202826b59740f26d
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-06-21 18:53:38 +0200

    Refactor multiple menus: Add onBoardUpdated and onCardUpdated methods for ViewBoard and ViewAdventureCard handling

--- src/main/java/it/polimi/ingsw/gc20/client/view/TUI/CargoMenu.java
+++ src/main/java/it/polimi/ingsw/gc20/client/view/TUI/CargoMenu.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.gc20.client.view.TUI;
 import it.polimi.ingsw.gc20.client.view.common.ViewLobby;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.ClientGameModel;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.GameModelListener;
+import it.polimi.ingsw.gc20.client.view.common.localmodel.adventureCards.ViewAdventureCard;
+import it.polimi.ingsw.gc20.client.view.common.localmodel.board.ViewBoard;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.components.ViewComponent;
 import it.polimi.ingsw.gc20.client.view.common.localmodel.ship.ViewShip;
 import it.polimi.ingsw.gc20.server.model.gamesets.CargoColor;
@@ -302,6 +304,17 @@ public class CargoMenu implements MenuState, GameModelListener {
 
     @Override
     public void onComponentInHandUpdated(ViewComponent component) {
+
+    }
+
+
+    @Override
+    public void onBoardUpdated(ViewBoard board) {
+
+    }
+
+    @Override
+    public void onCardUpdated(ViewAdventureCard card) {
 
     }
 }
