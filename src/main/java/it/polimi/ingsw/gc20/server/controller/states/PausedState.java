6d787e2d8921275a69edcf42a5eeb24047b02c12
Stefano Ungaro <stefano.ungaro.2003@gmail.com>
2025-06-23 13:52:21 +0200

    Refactor game states: Remove unnecessary Thread.sleep calls for improved responsiveness

--- src/main/java/it/polimi/ingsw/gc20/server/controller/states/PausedState.java
+++ src/main/java/it/polimi/ingsw/gc20/server/controller/states/PausedState.java
@@ -46,12 +46,12 @@ public class PausedState extends State {
         this.previousState = previousState;
         this.phase = StatePhase.STANDBY_PHASE;
         getController().getMessageManager().broadcastPhase(new StandbyMessage("Everyone disconnected. Waiting for the game to resume."));
-        // If nobody reconnects in 30 seconds, the remaining player wins the game
+        // If nobody reconnects in 90 seconds, the remaining player wins the game
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
         future = scheduler.schedule(() -> {
             getController().setState(new EndgameState(getController()));
             shutdown();
-        }, 120, TimeUnit.SECONDS);
+        }, 90, TimeUnit.SECONDS);
     }
 
     /**
